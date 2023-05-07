deaf99a3920fa69b6e5699a1914635a0428e07fa
duaal <duaalkhair@gmail.com>
2023-05-07 14:37:12 +0300

    Initial Commit

---
+++ CoachMe/CoachMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoachMeApp.swift
+//  CoachMe
+//
+//  Created by duaa mohammed on 07/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoachMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
